f626a9e7bafbf493815d7c90c97b3c01ec99a25f
WilliamSpanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-04-25 16:57:18 +0300

    Create a new InstaFollower class
    with methods for logging in, finding followers and following.

---
+++ insta_follower.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+class InstaFollower:
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 from export_credentials import ExportCredentials
+from insta_follower import InstaFollower
 
 setup_creds = ExportCredentials().setup_environment()
 
 INSTAGRAM_USERNAME = os.environ.get('INSTAGRAM_USERNAME')
 INSTAGRAM_PASSWORD = os.environ.get('INSTAGRAM_PASSWORD')
 TARGET_ACCOUNT = 'chefsteps'
+
+instaFollower = InstaFollower()
+instaFollower.login()
+instaFollower.find_followers()
+instaFollower.follow()
